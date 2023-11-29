e1cd409967c5a4bc3d06d53b4e40817b9b497fc0
Gabriel Nickel <gnickel@2-I-2.42heilbronn.de>
2023-11-29 19:56:31 +0100

    finished strmapi

--- 0.0-Libft/ft_strmapi.c
+++ 0.0-Libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: gnickel <gnickel@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 06:41:24 by gnickel           #+#    #+#             */
-/*   Updated: 2023/11/16 07:03:13 by gnickel          ###   ########.fr       */
+/*   Updated: 2023/11/29 19:55:40 by gnickel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 {
+	size_t	i;
+	size_t	len;
+	char	*res;
 
+	if (!s || !f)
+		return (0);
+	i = 0;
+	len = ft_strlen(s);
+	res = (char *)malloc((len + 1) * sizeof(char));
+	if (!res)
+		return (0);
+	while (i < len)
+	{
+		res[i] = f((unsigned int)i,s[i]);
+		i++;
+	}
+	res[len] = 0;
+	return (res);
 }
