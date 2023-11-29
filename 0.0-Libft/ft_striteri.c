4a297d375568ae2cc1837469e1204f3b6b97c314
Gabriel Nickel <gnickel@2-I-2.42heilbronn.de>
2023-11-29 20:03:37 +0100

    finished striteri

---
+++ 0.0-Libft/ft_striteri.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gnickel <gnickel@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 19:57:09 by gnickel           #+#    #+#             */
+/*   Updated: 2023/11/29 20:02:57 by gnickel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stddef.h>
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+	size_t	i;
+	size_t	len;
+	
+	if (!s || ! f)
+		return;
+	i = 0;
+	len = ft_strlen(s);
+	while (i < len)
+	{
+		f((unsigned int)i, &s[i]);
+		i++;
+	}
+}
