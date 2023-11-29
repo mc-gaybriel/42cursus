d59bd8be9dff6e9243f146ec5be18ffd58d23422
Gabriel Nickel <gnickel@2-I-2.42heilbronn.de>
2023-11-29 20:12:35 +0100

    finished putstr_fd

---
+++ 0.0-Libft/ft_putstr_fd.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gnickel <gnickel@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/29 20:08:11 by gnickel           #+#    #+#             */
+/*   Updated: 2023/11/29 20:11:05 by gnickel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <unistd.h>
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	len;
+
+	len = ft_strlen(s);
+	write(fd, s, len);
+}
