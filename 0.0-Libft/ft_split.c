5d3161f242cd40e99ae579c0c854fc8d726ba3e5
Gabriel Nickel <gnickel@2-G-3.42heilbronn.de>
2023-12-04 20:13:28 +0100

    fixed split

--- 0.0-Libft/ft_split.c
+++ 0.0-Libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: gnickel <gnickel@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 19:52:35 by gnickel           #+#    #+#             */
-/*   Updated: 2023/12/04 18:55:26 by gnickel          ###   ########.fr       */
+/*   Updated: 2023/12/04 20:12:59 by gnickel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 
 static int	count_words(const char *s, char c);
 static void	fill_res(const char *s, char **res, char c);
+static void	copy_word(char **res, const char *s, int *word_i, int word_len);
 void		free_split(char **split);
+
+typedef struct split_vars
+{
+	int	w_i;
+	int	w_len;
+	int	in_word;
+}	t_split_vars;
 
 char	**ft_split(const char *s, char c)
 {
@@ -51,33 +59,40 @@ static int	count_words(const char *s, char c)
 
 static void	fill_res(const char *s, char **res, char c)
 {
-	int	word_i;
-	int	word_len;
-	int	in_word;
+	t_split_vars	mc;
 
-	word_i = 0;
-	word_len = 0;
-	in_word = 0;
+	mc.w_i = 0;
+	mc.w_len = 0;
+	mc.in_word = 0;
 	while (*s)
 	{
 		if (*s == c)
 		{
-			if (in_word
-				&& (res[word_i] = (char *)malloc((word_len + 1) * sizeof(char))))
-				ft_strlcpy(res[word_i++], s - word_len, word_len + 1);
-			in_word = 0;
-			word_len = 0;
+			if (mc.in_word)
+				copy_word(res, s, &mc.w_i, mc.w_len);
+			mc.in_word = 0;
+			mc.w_len = 0;
 		}
-		else if (++in_word)
-			word_len++;
+		else
+		{
+			mc.in_word = 1;
+			if (mc.in_word)
+				mc.w_len++;
+		}
 		s++;
 	}
-	if (in_word && (res[word_i] = (char *)malloc((word_len +1) * sizeof(char))))
-	{
-		ft_strlcpy(res[word_i++], s - word_len, word_len + 1);
-		word_i++;
-	}
-	res[word_i] = 0;
+	if (mc.in_word)
+		copy_word(res, s, &mc.w_i, mc.w_len);
+	res[mc.w_i] = 0;
+}
+
+static void	copy_word(char **res, const char *s, int *word_i, int word_len)
+{
+	res[*word_i] = (char *)malloc((word_len + 1) * sizeof(char));
+	if (!res)
+		return;
+	if (res[*word_i])
+		ft_strlcpy(res[(*word_i)++], s - word_len, word_len + 1);
 }
 
 void	free_split(char **split)
